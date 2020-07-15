a7a3f301495c6dd0f41e7db664c473279839aacf
kaleaakash96 <56104967+kaleaakash96@users.noreply.github.com>
2020-07-15 22:13:42 +0530

    Update events.js

--- events.js
+++ events.js
@@ -1,25 +1,25 @@
 AFRAME.registerComponent('markerhandler', {
 
-     const animatedMarker = document.querySelector("#animated-marker");
+//      const animatedMarker = document.querySelector("#animated-marker");
 
-animatedMarker.addEventListener('click' ,() = > {
-    window.open("google.com",'_blank');
-});
+// animatedMarker.addEventListener('click' ,() = > {
+//     window.open("google.com",'_blank');
+// });
 
 
-});
+// });
 
-//     init: function() {
-//         const animatedMarker = document.querySelector("#animated-marker");
-//         const aEntity = document.querySelector("#animated-model");
+    init: function() {
+        const animatedMarker = document.querySelector("#animated-marker");
+        const aEntity = document.querySelector("#animated-model");
 
-//         // every click, we make our model grow in size :)
-//         animatedMarker.addEventListener('click', function(ev, target){
-//             const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
-//             if (aEntity && intersectedElement === aEntity) {
-// //                 const scale = aEntity.getAttribute('scale');
-// //                 Object.keys(scale).forEach((key) => scale[key] = scale[key] + 1);
-// //                 aEntity.setAttribute('scale', scale);
-//                             }
-//         });
-// }});
+        // every click, we make our model grow in size :)
+        animatedMarker.addEventListener('click', function(ev, target){
+            const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
+            if (aEntity && intersectedElement === aEntity) {
+//                 const scale = aEntity.getAttribute('scale');
+//                 Object.keys(scale).forEach((key) => scale[key] = scale[key] + 1);
+//                 aEntity.setAttribute('scale', scale);
+                            }
+        });
+}});
