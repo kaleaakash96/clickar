7b8a0cbdb3fc91c38438eeeade6fd0ac6a8a84e2
kaleaakash96 <56104967+kaleaakash96@users.noreply.github.com>
2020-07-15 22:59:36 +0530

    Update click.js

--- click.js
+++ click.js
@@ -1,8 +1,29 @@
-var buttons = document.querySelectorAll('button');
-for(var i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener('click', openNewTab, false);
-}
+// const clickBox85509 = document. getElementById ('clickBox85509');
+AFRAME.registerComponent('markerhandler', {
 
-function openNewTab(event) {
-   window.open(event.target.dataset.url, '_blank');
-}
+    init: function() {
+        const animatedMarker = document.querySelector("#animated-marker");
+        const aEntity = document.querySelector("#animated-model");
+
+        // every click, we make our model grow in size :)
+//         animatedMarker.addEventListener('click', function(ev, target){
+//             const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
+//             if (aEntity && intersectedElement === aEntity) {
+//                 const scale = aEntity.getAttribute('scale');
+//                 Object.keys(scale).forEach((key) => scale[key] = scale[key] + 1);
+//                 aEntity.setAttribute('scale', scale);
+//             }
+//         });
+        
+        
+        const popup = (() => {
+ open( "http://xahlee.info/js/js.html",
+ "_blank",
+ "width=640,height=480,menubar=no,scrollbar=no"); }
+);
+
+animatedMarker.addEventListener ( "click", popup, false);
+        
+        
+        
+}});
