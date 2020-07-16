aa64675e8676fcc68c97daf250d443e2e886c414
kaleaakash96 <56104967+kaleaakash96@users.noreply.github.com>
2020-07-17 00:38:30 +0530

    Create clk2.js

---
+++ clk2.js
@@ -0,0 +1,30 @@
+  
+// const clickBox85509 = document. getElementById ('clickBox85509');
+AFRAME.registerComponent('markerhandler', {
+
+    init: function() {
+        const animatedMarker = document.querySelector("#animated-marker");
+        const aEntity = document.querySelector("#animated-model");
+
+        // every click, we make our model grow in size :)
+//         animatedMarker.addEventListener('click', function(ev, target){
+//             const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
+//             if (aEntity && intersectedElement === aEntity) {
+//                 const scale = aEntity.getAttribute('scale');
+//                 Object.keys(scale).forEach((key) => scale[key] = scale[key] + 1);
+//                 aEntity.setAttribute('scale', scale);
+//             }
+//         });
+        
+        
+        const popup = (() => {
+ open( "https://www.google.com",
+ "_blank",
+ "width=640,height=480,menubar=no,scrollbar=no"); }
+);
+
+animatedMarker.addEventListener ( "click", popup, false);
+        
+        
+        
+}});
